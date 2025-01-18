0290d7bdbe17034d579c31cfbcd684bcfd867a8a
IldenH <IldenH.1@proton.me>
2025-01-18 20:00:17 +0100

    picker: cleaner binds

--- config/picker.nix
+++ config/picker.nix
@@ -9,21 +9,18 @@
       setupOpts."@" = "default-title";
 
       keys = [
-        # Files
-        (util.mkKeymap "n" "<leader>fF" ":FzfLua<cr>" "Picker picker")
-        (util.mkKeymap "n" "<leader>ff" ":FzfLua files<cr>" "Files")
-        (util.mkKeymap "n" "<leader>fo" ":FzfLua oldfiles<cr>" "File history")
-        (util.mkKeymap "n" "<leader>fl" ":FzfLua blines<cr>" "Lines")
-        (util.mkKeymap "n" "<leader>fs" ":FzfLua live_grep_glob<cr>" "Grep")
-        (util.mkKeymap "n" "<leader>fS" ":FzfLua live_grep_resume<cr>" "Last grep")
-        (util.mkKeymap "n" "<leader>fb" ":FzfLua lgrep_curbuf<cr>" "Grep buffer")
-        # Git
-        (util.mkKeymap "n" "<leader>gs" ":FzfLua git_status<cr>" "Status")
-        (util.mkKeymap "n" "<leader>gc" ":FzfLua git_commits<cr>" "Commits")
-        (util.mkKeymap "n" "<leader>gC" ":FzfLua git_bcommits<cr>" "Buffer commits")
-        (util.mkKeymap "n" "<leader>gb" ":FzfLua git_blame<cr>" "Blame")
-        (util.mkKeymap "n" "<leader>gB" ":FzfLua git_branches<cr>" "Branches")
-        (util.mkKeymap "n" "<leader>gs" ":FzfLua git_stash<cr>" "Stash")
+        (util.mkKeymap "n" "<leader>fF" ":FzfLua<cr>" " Picker")
+        (util.mkKeymap "n" "<leader>ff" ":FzfLua files<cr>" " Find")
+        (util.mkKeymap "n" "<leader>fs" ":FzfLua live_grep_glob<cr>" " Search")
+        (util.mkKeymap "n" "<leader>fb" ":FzfLua lgrep_curbuf<cr>" " Search buffer")
+
+        (util.mkKeymap "n" "<leader>gg" ":FzfLua git_files<cr>" " Find")
+        (util.mkKeymap "n" "<leader>gc" ":FzfLua git_commits<cr>" " Commits")
+        (util.mkKeymap "n" "<leader>gC" ":FzfLua git_bcommits<cr>" " Buffer commits")
+        (util.mkKeymap "n" "<leader>gs" ":FzfLua git_status<cr>" "󱖫 Status")
+        (util.mkKeymap "n" "<leader>gb" ":FzfLua git_blame<cr>" " Blame")
+        (util.mkKeymap "n" "<leader>gB" ":FzfLua git_branches<cr>" " Branches")
+
         # LSP: TODO
         # Other
         (util.mkKeymap "n" "<leader>fc" ":FzfLua command_history<cr>" "Command history")
